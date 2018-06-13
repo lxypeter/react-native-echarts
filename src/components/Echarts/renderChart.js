f26fe48722d3126b5e1b02f520cc64c9d9cf17a6
CY.Lee <lxypeter@sina.com>
2018-06-13 17:31:33 +0800

    suit for the width

--- src/components/Echarts/renderChart.js
+++ src/components/Echarts/renderChart.js
@@ -3,8 +3,15 @@ import toString from '../../util/toString';
 
 export default function renderChart(props) {
   const height = props.height || 400;
+  const width = props.width;
+  let mainWidth = '';
+  if (width) {
+    mainWidth = `document.getElementById('main').style.width = "${width}px";`;
+  }
+  console.log(props.option);
   return `
     document.getElementById('main').style.height = "${height}px";
+    ${mainWidth}
     var myChart = echarts.init(document.getElementById('main'));
     myChart.setOption(${toString(props.option)});
   `
